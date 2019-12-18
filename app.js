a59150cb241cbbdfd35192f9af36614cc2f10979
tandavala <jose.tandavala@gmail.com>
2019-12-18 20:52:21 +0100

    getting item from local storage

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // store data in local storage
 localStorage.setItem("name", "Jose");
 localStorage.setItem("age", "30");
+
 // get data from local storage
 let name = localStorage.getItem("name");
-console.log(name);
+let age = localStorage.getItem("age");
+console.log(`${name} ${age}`);
+
 // updating data
+localStorage.setItem("name", "Uriel");
+localStorage.age = 7;
+name = localStorage.getItem("name");
+age = localStorage.getItem("age");
+console.log(name);
+console.log(age);
