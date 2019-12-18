5fd493e81e902daf86715db644daeea7de42772b
tandavala <jose.tandavala@gmail.com>
2019-12-18 21:07:58 +0100

    saving objcts into a localstore and retrive the data stored.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/*const myList = document.querySelector("ul");
+
 // store data in local storage
 localStorage.setItem("name", "Jose");
-localStorage.setItem("age", "30");
+localStorage.setItem("age", "27");
 
 // get data from local storage
 let name = localStorage.getItem("name");
@@ -14,3 +16,28 @@ name = localStorage.getItem("name");
 age = localStorage.getItem("age");
 console.log(name);
 console.log(age);
+
+// Delete item from localstorage
+
+//localStorage.removeItem("name");
+//localStorage.clear();
+
+name = localStorage.getItem("name");
+age = localStorage.getItem("age");
+let html = `
+   <li>Name:${name}, Age: ${age}</li> 
+`;
+myList.innerHTML += html;
+*/
+
+const todos = [
+  { text: "play mariokart", author: "shaum" },
+  { text: "study javascript", author: "Caelum" },
+  { text: "learning react js", author: "Humberto" }
+];
+
+//console.log(JSON.stringify(todos));
+localStorage.setItem("todos", JSON.stringify(todos));
+
+const stored = localStorage.getItem("todos");
+console.log(JSON.parse(stored));
