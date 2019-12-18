d2e3caf16942c73d2f6c2d6f3baae306e6725998
tandavala <jose.tandavala@gmail.com>
2019-12-18 20:45:59 +0100

    saving data to localstorage

---
+++ app.js
@@ -0,0 +1,7 @@
+// store data in local storage
+localStorage.setItem("name", "Jose");
+localStorage.setItem("age", "30");
+// get data from local storage
+let name = localStorage.getItem("name");
+console.log(name);
+// updating data
